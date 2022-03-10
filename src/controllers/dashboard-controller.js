c4a3b4011ef102174027129e5382d5149cf2986b
David Fagan <david.fagan1989@gmail.com>
2022-03-10 18:25:07 +0000

    added accounts controller and edit dashboard cont

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
-    index: {
-      handler: async function (request, h) {
-        return h.view("main");
-      },
+  index: {
+    handler: async function (request, h) {
+      const megalithicMonuments = await db.megalithicStore.getAllMegalithicMonuments();
+      const viewData = {
+        title: "Megalithic Ireland Dashboard",
+        megalithicMonuments: megalithicMonuments,
+      };
+      return h.view("dashboard-view", viewData);
     },
-  };
+  },
+
+  addMegalithicMonument: {
+    handler: async function (request, h) {
+      const newMegalithicMonument = {
+        name: request.payload.title,
+      };
+      await db.megalithicStore.addMegalithicMonument(newMegalithicMonument);
+      return h.redirect("/dashboard");
+    },
+  },
+};
