21f95f6ea42a8d24174def50997e1d44e77c7890
David Fagan <david.fagan1989@gmail.com>
2022-03-11 14:48:05 +0000

    create function and route add megalithic

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,3 +1,4 @@
+import { megalithicApi } from "./api/megalithic-api.js";
 import { userApi } from "./api/user-api.js";
 
 export const apiRoutes = [
@@ -5,4 +6,6 @@ export const apiRoutes = [
   { method: "GET", path: "/api/users", config: userApi.find },
   { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
   { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+
+  { method: "POST", path: "/api/megalithicmonuments", config: megalithicApi.create }
 ];
