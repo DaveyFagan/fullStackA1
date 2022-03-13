7120ca03315e54d81092ee72b77722606abfd0d2
David Fagan <david.fagan1989@gmail.com>
2022-03-13 13:00:06 +0000

    getAllMegalitihic function

--- src/models/json/megalithic-json-store.js
+++ src/models/json/megalithic-json-store.js
@@ -1,3 +1,13 @@
 import { v4 } from "uuid";
 import { JSONFile, Low } from "lowdb";
 
+const db = new Low(new JSONFile("./src/models/json/megalithics.json"));
+db.data = { megalithics: [] };
+
+export const megalithicJsonStore = {
+    async getAllMegalithics() {
+        await db.read();
+        return db.data.megalithics;
+    },
+}
+
