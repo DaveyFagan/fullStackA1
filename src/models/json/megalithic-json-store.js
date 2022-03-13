94233bca0c3564264f66c6735595d924d1caac99
David Fagan <david.fagan1989@gmail.com>
2022-03-13 13:02:51 +0000

    addMegalithic func

--- src/models/json/megalithic-json-store.js
+++ src/models/json/megalithic-json-store.js
@@ -9,5 +9,13 @@ export const megalithicJsonStore = {
         await db.read();
         return db.data.megalithics;
     },
+
+    async addMegalithic(megalithic) {
+        await db.read();
+        megalithic._id = v4();
+        db.data.megalithics.push(megalithic);
+        await db.write();
+        return megalithic;
+    },
 }
 
