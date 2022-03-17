53a63e3cb0c99b21ed7edace7b823bdc2b80c81e
David Fagan <david.fagan1989@gmail.com>
2022-03-17 19:44:54 +0000

    refactored user mem functions

--- src/models/db.js
+++ src/models/db.js
@@ -1,14 +1,14 @@
-/*
+
 import { userMemStore } from "./mem/user-mem-store.js";
 import { monumentMemStore } from "./mem/monument-mem-store.js";
 import { placeMemStore } from "./mem/place-mem-store.js";
-*/
 
 
+/*
 import { userJsonStore } from "./json/user-json-store.js";
 import { monumentJsonStore } from "./json/monument-json-store.js";
 import { placeJsonStore } from "./json/place-json-store.js";
-
+*/
 
 export const db = {
   userStore: null,
@@ -16,15 +16,15 @@ export const db = {
   placeStore: null,
 
   init() {
-    
+   /* 
     this.userStore = userJsonStore;
     this.monumentStore = monumentJsonStore;
     this.placeStore = placeJsonStore;
-    
-  /*
+   */ 
+  
     this.userStore = userMemStore;
     this.monumentStore = monumentMemStore;
     this.placeStore = placeMemStore;
-  */  
+    
   },
 };
