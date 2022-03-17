02f1bb1166659472cd23447bc002a8178786f586
David Fagan <david.fagan1989@gmail.com>
2022-03-17 21:19:10 +0000

    monumet test

--- test/models/monument-model-test.js
+++ test/models/monument-model-test.js
@@ -1,22 +1,29 @@
 import { assert } from "chai";
-import { db } from "../src/models/db.js";
-import { testMonuments, newbridge } from "./fixtures.js";
+import { db } from "../../src/models/db.js";
+import { testMonuments, newbridge, dublin } from "../fixtures.js";
+import { assertSubset } from "../test-utils.js";
+
 
 suite("Monument Model tests", () => {
 
+    let monumentList = null;
+
   setup(async () => {
     db.init("");
+    await db.placeStore.deleteAllPlaces();
+    monumentList = await db.placeStore.addPlace(dublin)
     await db.monumentStore.deleteAllMonuments();
     for (let i = 0; i < testMonuments.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
-      testMonuments[i] = await db.monumentStore.addMonument(testMonuments[i]);
+      testMonuments[i] = await db.monumentStore.addMonument(monumentList._id, testMonuments[i]);
     }
   });
 
-  test("create a Monument", async () => {
-    const monument = await db.monumentStore.addMonument(newbridge);
-    assert.equal(newbridge, monument);
-    assert.isDefined(monument._id);
+  test("create a single Monument", async () => {
+    const dublinList = await db.placeStore.addPlace(dublin)
+    const monument = await db.monumentStore.addMonument(dublinList._id, newbridge);
+    assert.isNotNull(monument._id);
+    assertSubset (newbridge, monument);
   });
 /*
   test("delete all Monuments", async () => {
