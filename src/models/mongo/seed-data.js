2469415bb6223fc16073396d45a775196757091a
David Fagan <david.fagan1989@gmail.com>
2022-03-24 21:05:51 +0000

    seeding mongo

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,75 @@
+export const seedData = {
+    users: {
+      _model: "User",
+      homer: {
+        firstName: "Homer",
+        lastName: "Simpson",
+        email: "homer@simpson.com",
+        password: "secret"
+      },
+      marge: {
+        firstName: "Marge",
+        lastName: "Simpson",
+        email: "marge@simpson.com",
+        password: "secret"
+      },
+      bart: {
+        firstName: "Bart",
+        lastName: "Simpson",
+        email: "bart@simpson.com",
+        password: "secret"
+      },
+      admin: {
+        firstName: "Special",
+        lastName: "User",
+        email: "admin@mail.com",
+        password: "secret"
+      }
+    },
+
+    places: {
+        _model: "Place",
+        meath: {
+          name: "Meath",
+          userid: "->users.bart"
+        },
+        sligo: {
+          name: "Sligo",
+          userid: "->users.homer"
+          },
+        roscommon: {
+          name: "Roscommon",
+          userid: "->users.marge"
+          },
+      },
+
+
+
+      monuments: {
+        _model : "Monument",
+        monuments : {
+          name: "Newgrange",
+          description: "5,200 year old passage tomb located in the Boyne Valley in Ireland's Ancient East",
+          lat: 53,
+          lng: -6,
+          cat: "Passage Tomb",
+          placeid: "->places.meath"
+        },
+        monument2 : {
+            name: " The Cairn of Queen Maeve ",
+            description: "On the summit of Knocknarea Mountain (320 meters in height) in County Sligo, a large Cairn can be found known locally as Miosgan Meadhba (Maeves Cairn)",
+            lat: 54,
+            lng: -8,
+            cat: "Cairn",
+            placeid: "->places.sligo"
+          },
+            monument3 : {
+            name: "Ballincool Standing Stone",
+            description: "This enormous standing stone is very impressive and can be seen quite clearly from the road",
+            lat: 51,
+            lng: 14,
+            cat: "Standing Stone",
+            placeid: "->places.roscommon"
+          },
+      }
+  };
--- test/models/place-model-test.js
+++ test/models/place-model-test.js
@@ -6,7 +6,7 @@ import { assertSubset } from "../test-utils.js";
 suite("Place Model tests", () => {
 
   setup(async () => {
-    db.init("json");
+    db.init("mongo");
     await db.placeStore.deleteAllPlaces();
     for (let i = 0; i < testPlaces.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
