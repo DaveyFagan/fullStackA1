80f5f169481822abab5766006579557e4c96e5e3
David Fagan <david.fagan1989@gmail.com>
2022-03-29 13:58:01 +0100

    ser

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ import { accountsController } from "./controllers/accounts-controller.js";
 const result = dotenv.config();
 if (result.error) {
   console.log(result.error.message);
-  process.exit(1);
+  // process.exit(1);
 }
 
 const swaggerOptions = {
