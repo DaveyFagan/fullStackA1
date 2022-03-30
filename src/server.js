41766c67903eab515aa234ff27f07d40e40e9ca7
David Fagan <david.fagan1989@gmail.com>
2022-03-30 16:00:37 +0100

    define a new strategy

--- src/server.js
+++ src/server.js
@@ -57,6 +57,11 @@ async function init() {
     redirectTo: "/",
     validateFunc: accountsController.validate,
   });
+  server.auth.strategy("jwt", "jwt", {
+    key: process.env.cookie_password,
+    validate: validate,
+    verifyOptions: { algorithms: ["HS256"] }
+  });
   server.auth.default("session");
   server.views({
     engines: {
